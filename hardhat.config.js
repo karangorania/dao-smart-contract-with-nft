8611b9b11daf71749349cfbb003ef240fb0ed807
karangorania <karangoraniya@gmail.com>
2022-07-05 13:05:49 +0530

    gas optimizer & mumbai network added 🚀

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,35 @@
 require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('dotenv').config();
+
+const { POLYGON_API_URL, PRIVATE_KEY, POLYGON_SCAN_KEY } = process.env;
 
 module.exports = {
-  solidity: '0.8.4',
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      gas: 12000000,
+      gasPrice: 8000000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000,
+    },
+    mumbai: {
+      url: POLYGON_API_URL,
+      accounts: {
+        mnemonic: PRIVATE_KEY,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: POLYGON_SCAN_KEY,
+  },
 };
